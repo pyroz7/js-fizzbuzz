8c637a479f6073cd3da5bc74cc15b27010353127
pyroz7 <vincipyroz7@gmail.com>
2023-11-15 14:45:40 +0100

    Half

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*Stampiamo i numeri da 1 a 100*/
+
+for (let i = 0; i < 100; i++) {
+
+
+/*Dobbiamo suddividerli in multipli di 3 e 5 scrivendo un testo affianco ai numeri con i multipli richiesti*/
+
+if (i % 3 == 0 ) {
+
+    console.log(i + " - Fizz");
+
+} 
+
+else if (i % 5 == 0) {
+    
+    console.log(i + " - Buzz");
+
+}
+
+    
+}
+
+
+
+

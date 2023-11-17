006f382914dd5d590327463cd5013d43618102bf
pyroz7 <vincipyroz7@gmail.com>
2023-11-17 18:07:12 +0100

    Finish

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 /*Stampiamo i numeri da 1 a 100*/
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0 ; i < 101; i++) {
 
 
 /*Dobbiamo suddividerli in multipli di 3 e 5 scrivendo un testo affianco ai numeri con i multipli richiesti*/
 
-if (i % 3 == 0 ) {
+if ( i % 5 == 0 && i % 3 == 0 ) {
 
-    console.log(i + " - Fizz");
+    console.log(i + " - FizzBuzz");
 
 } 
 
@@ -15,6 +15,16 @@ else if (i % 5 == 0) {
     
     console.log(i + " - Buzz");
 
+}
+
+else if (i % 3 == 0) {
+
+    console.log(i + " - Fizz");
+
+} 
+
+else {
+    console.log(i);
 }
 
     
